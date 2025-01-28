00957a97dbe8bc16e8b76ef338fdc3371fd1346f
itheamc <itheamc@gmail.com>
2025-01-28 10:38:53 +0545

    [#16] deleted duplicated file

--- app/src/main/java/np/com/naxa/drone_tasking_manager/features/projects/mapper/ProjectsResponseMapper.kt
+++ app/src/main/java/np/com/naxa/drone_tasking_manager/features/projects/mapper/ProjectsResponseMapper.kt
@@ -1,19 +1,14 @@
 package np.com.naxa.drone_tasking_manager.features.projects.mapper
 
-import np.com.naxa.drone_tasking_manager.features.projects.dto.Outline
-import np.com.naxa.drone_tasking_manager.features.projects.dto.ProjectsResponseDto
-import np.com.naxa.drone_tasking_manager.features.projects.dto.Result
+import np.com.naxa.drone_tasking_manager.features.projects.dto.project.Outline
+import np.com.naxa.drone_tasking_manager.features.project_details.dto.project.ProjectResponseDto
+import np.com.naxa.drone_tasking_manager.features.projects.dto.projects.ProjectsResponseDto
+import np.com.naxa.drone_tasking_manager.features.projects.dto.projects.Result
 import np.com.naxa.drone_tasking_manager.features.projects.models.Project
 import np.com.naxa.drone_tasking_manager.features.projects.models.ProjectGeometry
 import np.com.naxa.drone_tasking_manager.features.projects.models.ProjectsResponse
 
-
-fun Outline.toProjectGeometry() = ProjectGeometry(
-    type = type,
-    coordinates = coordinates
-)
-
-fun Result.toProject() = Project(
+fun ProjectResponseDto.toProject() = Project(
     id = id,
     slug = slug,
     name = name,
@@ -40,10 +35,45 @@ fun Result.toProject() = Project(
     authorId = authorId,
 )
 
+fun Result.toProject() = Project(
+    id = id,
+    slug = slug,
+    name = name,
+    description = description,
+    perTaskInstructions = perTaskInstructions,
+    requiresApprovalFromManagerForLocking = requiresApprovalFromManagerForLocking,
+    outline = ProjectGeometry(
+        geometry = outline,
+    ),
+    noFlyZones = null,
+    requiresApprovalFromRegulator = requiresApprovalFromRegulator,
+    regulatorEmails = regulatorEmails,
+    regulatorApprovalStatus = regulatorApprovalStatus,
+    imageProcessingStatus = imageProcessingStatus,
+    regulatorComment = regulatorComment,
+    commentingRegulatorId = commentingRegulatorId,
+    authorName = authorName,
+    projectArea = projectArea,
+    totalTaskCount = totalTaskCount,
+    imageUrl = imageUrl,
+    ongoingTaskCount = ongoingTaskCount,
+    completedTaskCount = completedTaskCount,
+    status = status,
+    createdAt = createdAt,
+    authorId = authorId,
+)
+
 fun ProjectsResponseDto.toProjectResponse() = ProjectsResponse(
     hasNext = pagination?.hasNext,
     hasPrev = pagination?.hasPrev,
     page = pagination?.page,
     total = pagination?.total,
     projects = results.map { it.toProject() }.toList()
+)
+
+fun Outline.toProjectGeometry() = ProjectGeometry(
+    type = type,
+    geometry = geometry,
+    properties = properties,
+    id = id,
 )
