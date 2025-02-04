4996b6178a1769a12d0c1ea3b14fd751faa854b6
Samir Dangal <samirdangal@gmail.com>
2025-02-04 09:10:43 +0545

    [#23] logout functionality added (#26)
    * [#23] added method to clear all view models
    
    * [#23] added logout feature
    
    * [#23] minor UI enhancement
    
    * [#23] minor UI enhancement
    
    * [#23] minor UI enhancement

--- app/src/main/java/np/com/naxa/drone_tasking_manager/app/DroneTMApp.kt
+++ app/src/main/java/np/com/naxa/drone_tasking_manager/app/DroneTMApp.kt
@@ -1,5 +1,9 @@
 package np.com.naxa.drone_tasking_manager.app
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,7 +14,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.LineStyle
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -36,22 +39,25 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 import np.com.naxa.drone_tasking_manager.R
 import np.com.naxa.drone_tasking_manager.Routes
+import np.com.naxa.drone_tasking_manager.core.services.storage.MMKVStorageService
 import np.com.naxa.drone_tasking_manager.core.theme.DroneTMAppTheme
+import np.com.naxa.drone_tasking_manager.core.utils.clearAllViewModels
 import np.com.naxa.drone_tasking_manager.features.login.viewmodels.LoginViewModel
-import np.com.naxa.drone_tasking_manager.features.user.profile.viewmodels.UserProfileViewModel
 import np.com.naxa.drone_tasking_manager.features.project_details.viewmodels.ProjectDetailViewModel
 import np.com.naxa.drone_tasking_manager.features.projects.viewmodels.ProjectsViewModel
 import np.com.naxa.drone_tasking_manager.features.tasks.viewmodels.TasksViewModel
 import np.com.naxa.drone_tasking_manager.features.user.auth.refreshtoken.viewmodels.RefreshTokenViewModel
+import np.com.naxa.drone_tasking_manager.features.user.profile.viewmodels.UserProfileViewModel
 import np.com.naxa.drone_tasking_manager.local_providers.LocalDownloadAndTransferFileViewModel
 import np.com.naxa.drone_tasking_manager.local_providers.LocalEventsViewModel
 import np.com.naxa.drone_tasking_manager.local_providers.LocalLoginViewModel
@@ -96,6 +102,9 @@ fun DroneTMApp(
             backStackEntry?.destination?.route?.route()
         }
     }
+
+    val viewModels = LocalViewModelStoreOwner.current!!
+    val context = LocalContext.current
 
     val loginViewModel = hiltViewModel<LoginViewModel>()
     val userProfileViewModel = hiltViewModel<UserProfileViewModel>()
@@ -228,6 +237,7 @@ fun DroneTMApp(
                 topBar = {
                     if (currentRoute != Routes.Splash
                         && currentRoute != Routes.Home
+                        && currentRoute != Routes.Login
                         && currentRoute != Routes.ProjectsMap
                         && currentRoute != Routes.ProjectDetails
                     ) {
@@ -286,10 +296,9 @@ fun DroneTMApp(
                             },
 
                             actions = {
+                                if(currentRoute == Routes.ProjectsList){
                                 IconButton(onClick = {
-                                    navigationEventsViewModel.sendEvent(
-                                        DroneTMAppNavigationEvent.OnNavigateToProfileScreen
-                                    )
+                                    menuExpanded = !menuExpanded
                                 }) {
                                     Surface(
                                         shape = CircleShape,
@@ -305,6 +314,30 @@ fun DroneTMApp(
                                             modifier = Modifier.padding(4.dp),
                                         )
                                     }
+                                }
+                                }
+
+
+                                // Dropdown menu
+                                DropdownMenu(
+                                    expanded = menuExpanded,
+                                    onDismissRequest = { menuExpanded = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = { Text("Profile") },
+                                        onClick = {
+                                            onProfileClick(navigationEventsViewModel)
+                                            menuExpanded = false
+                                        }
+                                    )
+                                    DropdownMenuItem(
+                                        text = { Text("Logout") },
+                                        onClick = {
+
+                                            onLogout(navigationEventsViewModel, viewModels, context)
+                                            menuExpanded = false
+                                        }
+                                    )
                                 }
 
                             },
@@ -353,4 +386,34 @@ fun DroneTMApp(
         }
 
     }
+}
+
+fun onLogout(
+    navigationEventsViewModel: NavigationEventsViewModel,
+    viewModels: ViewModelStoreOwner,
+    context: Context
+) {
+
+    MMKVStorageService.getInstance().clear()
+
+    clearAllViewModels(viewModels)
+
+    //restart the app
+    val packageManager: PackageManager = context.packageManager
+    val intent: Intent = packageManager.getLaunchIntentForPackage(context.packageName)!!
+    val componentName: ComponentName = intent.component!!
+    val restartIntent: Intent = Intent.makeRestartActivityTask(componentName)
+    context.startActivity(restartIntent)
+    Runtime.getRuntime().exit(0)
+
+//    navigationEventsViewModel.sendEvent(
+//        DroneTMAppNavigationEvent.OnNavigateToLogin
+//    )
+
+}
+
+fun onProfileClick(navigationEventsViewModel: NavigationEventsViewModel) {
+    navigationEventsViewModel.sendEvent(
+        DroneTMAppNavigationEvent.OnNavigateToProfileScreen
+    )
 }
