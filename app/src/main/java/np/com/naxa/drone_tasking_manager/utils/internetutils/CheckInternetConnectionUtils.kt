9017c71caf517d073e1e13a0b5cf9c49c14c025b
Samir Dangal <samirdangal@gmail.com>
2025-02-13 17:35:09 +0545

    [#58] added internet connection utils

--- app/src/main/java/np/com/naxa/drone_tasking_manager/utils/internetutils/CheckInternetConnectionUtils.kt
+++ app/src/main/java/np/com/naxa/drone_tasking_manager/utils/internetutils/CheckInternetConnectionUtils.kt
@@ -4,7 +4,16 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
+/**
+ * Object providing utility functions for checking internet connectivity.
+ */
 object CheckInternetConnectionUtils {
+    /**
+     * Checks if the device has an active internet connection.
+     *
+     * @param context The application context.
+     * @return True if the device has internet connectivity, false otherwise.
+     */
     fun isInternetAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
