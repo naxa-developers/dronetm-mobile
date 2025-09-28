c4e6850f967d4a130f0ad85f7399bf73dcb9277c
spwoodcock <sam.woodcock@protonmail.com>
2025-09-28 14:47:18 +0100

    build: use container for manual prod apk scripts

--- scripts/gen_keystore.sh
+++ scripts/gen_keystore.sh
@@ -4,12 +4,19 @@ set -euo pipefail
 # NOTE for PKCS12 keystores, the store pass and key pass will be the same
 : "${PASSWORD:?Environment variable PASSWORD must be set}"
 
-keytool -genkeypair -v \
-  -keystore scripts/hotosm.jks \
-  -alias hotosm-android \
-  -keyalg RSA \
-  -keysize 2048 \
-  -validity 10000 \
-  -storepass "$PASSWORD" \
-  -keypass "$PASSWORD" \
-  -dname "CN=HOT, OU=Dev, O=Humanitarian OpenStreetMap Team, L=Global, ST=Earth, C=US"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+docker run --rm \
+  -v "$PROJECT_DIR":"$PROJECT_DIR" \
+  --workdir "$PROJECT_DIR" \
+  docker.io/cimg/android:2025.09 \
+  keytool -genkeypair -v \
+    -keystore "$SCRIPT_DIR/hotosm.jks" \
+    -alias hotosm-android \
+    -keyalg RSA \
+    -keysize 2048 \
+    -validity 10000 \
+    -storepass "$PASSWORD" \
+    -keypass "$PASSWORD" \
+    -dname "CN=HOT, OU=Dev, O=Humanitarian OpenStreetMap Team, L=Global, ST=Earth, C=US"
