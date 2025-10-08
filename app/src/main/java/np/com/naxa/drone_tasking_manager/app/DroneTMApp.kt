3f934d0704a6918655471f4b5d659e794e1502fa
itheamc <itheamc@gmail.com>
2025-10-08 16:15:51 +0545

    [#75] refactor hilt viewmodel related import

--- app/src/main/java/np/com/naxa/drone_tasking_manager/app/DroneTMApp.kt
+++ app/src/main/java/np/com/naxa/drone_tasking_manager/app/DroneTMApp.kt
@@ -50,7 +50,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.compose.rememberNavController
